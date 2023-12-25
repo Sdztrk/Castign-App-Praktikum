006308c833fcda0d2e1fea68c08e1416fd5457a9
Hdemiribram <passoatacc5862g@mail.com>
2023-12-25 09:48:35 +0300

    genel kart tasarımı tamamlandı

--- component/layout/layout.js
+++ component/layout/layout.js
@@ -1,10 +1,15 @@
 import ResponsiveAppBar from './navbar'
 import Footer from './footer'
+import CategoryCard from './CategoryCard'
+import GeneralCard from './GeneralCard'
+
  
 export default function Layout({ children }) {
   return (
     <>
       <ResponsiveAppBar />
+      <GeneralCard/>
+      <CategoryCard/>
       <main>{children}</main>
       <Footer />
     </>
