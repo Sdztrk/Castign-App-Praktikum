5a68dd73db0a6734bec5d17416b2f6b79564d41f
Ugur <u.s.cakmak02@gmail.com>
2023-12-29 23:18:51 +0100

    updated API class & added JWT usage

--- helpers/api_builder.js
+++ helpers/api_builder.js
@@ -1,20 +1,85 @@
 const BASE_URL = "http://localhost:8000/api/";
 
+/**
+ * API class to handle HTTP requests.
+ */
 class API {
-    static async get(endpoint) {
-        const response = await fetch(BASE_URL + endpoint);
-        return response.json();
+    /**
+     * Perform a GET request to the specified endpoint.
+     * @param {string} endpoint - The endpoint to send the GET request to.
+     * @param {string|null} token - Optional token for authorization.
+     * @returns {Promise<any>} - A promise that resolves with the response data.
+     * @throws {Error} - If there is an HTTP error or a network error.
+     */
+    static async get(endpoint, token = null) {
+        // Define headers with default Content-Type
+        const headers = {
+            'Content-Type': 'application/json',
+        };
+
+        // Add Authorization header if token is provided
+        if (token) {
+            headers['Authorization'] = `Bearer ${token}`;
+        }
+
+        try {
+            // Perform the GET request
+            const response = await fetch(BASE_URL + endpoint, {
+                method: 'GET',
+                headers: headers,
+            });
+
+            // Check if response is successful
+            if (!response.ok) {
+                console.log(`HTTP error! Status: ${response.status}`);
+            }
+
+            // Parse and return the JSON response
+            return response.json();
+        } catch (error) {
+            // Log the error and re-throw it
+            console.error("Error fetching data:", error);
+        }
     }
 
-    static async post(endpoint, data) {
-        const response = await fetch(BASE_URL + endpoint, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: new URLSearchParams(data).toString(),
-        });
-        return response.json();
+    /**
+     * Perform a POST request to the specified endpoint.
+     * @param {string} endpoint - The endpoint to send the POST request to.
+     * @param {Object} data - The data to send in the POST request body.
+     * @param {string|null} token - Optional token for authorization.
+     * @returns {Promise<any>} - A promise that resolves with the response data.
+     * @throws {Error} - If there is an HTTP error or a network error.
+     */
+    static async post(endpoint, data, token = null) {
+        // Define headers with Content-Type for form data
+        const headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        };
+
+        // Add Authorization header if token is provided
+        if (token) {
+            headers['Authorization'] = `Bearer ${token}`;
+        }
+
+        try {
+            // Perform the POST request
+            const response = await fetch(BASE_URL + endpoint, {
+                method: 'POST',
+                headers: headers,
+                body: new URLSearchParams(data).toString(),
+            });
+
+            // Check if response is successful
+            if (!response.ok) {
+                console.log(`HTTP error! Status: ${response.status}`);
+            }
+
+            // Parse and return the JSON response
+            return response.json();
+        } catch (error) {
+            // Log the error and re-throw it
+            console.error("Error posting data:", error);
+        }
     }
 }
 
