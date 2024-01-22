c99a18290167b3b0644124ec01923e4f0d99a782
Ugur <u.s.cakmak02@gmail.com>
2024-01-22 22:50:58 +0100

    added profile page

--- component/layout/navbar.js
+++ component/layout/navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -17,6 +17,7 @@ import { mainColor } from "@/constants/Colors";
 import API from '@/helpers/ApiBuilder';
 import AppContext from '@/AppContext';
 import Cookies from 'js-cookie';
+import { BackendMediaPath } from '@/constants/BackendValues';
 
 
 
@@ -28,14 +29,17 @@ function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
   const [user, setUser] = useState(null);
+  const [photoPath, setPhotoPath] = useState("/static/images/avatar/2.jpg")
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const curentUser = await API.get('get_current_user', Cookies.get("accessToken"));
+        console.log(curentUser)
         if (curentUser) {
           setUser(curentUser?.email)
           setUserInfo({user: curentUser, loggedIn: curentUser?.email ? true : false})
+          setPhotoPath(BackendMediaPath + curentUser?.photo)
         }
       } catch (error) {
         console.error("Error:", error);
@@ -168,7 +172,7 @@ function ResponsiveAppBar() {
             <Box sx={{ flexGrow: 0 }}>
               <Tooltip title="Open settings">
                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                  <Avatar alt="Remy Sharp" src={photoPath} />
                 </IconButton>
               </Tooltip>
               <Menu
