797e0763a83c64081272dd7fd23a681322024ae0
Hdemiribram <passoatacc5862g@mail.com>
2024-01-31 16:24:48 +0300

    resimlerle ilili bir hata geliyordu alt etiketi eklenmediği için olmayanlara eklendi

--- component/cards/CategoryCard.js
+++ component/cards/CategoryCard.js
@@ -1,73 +1,85 @@
-import React, { useState } from 'react'
-import Card from '@mui/material/Card'
-import CardContent from '@mui/material/CardContent'
-import CardMedia from '@mui/material/CardMedia'
-import Typography from '@mui/material/Typography'
-import Tooltip from '@mui/material/Tooltip'
-import { useRouter } from 'next/navigation'
+import React, { useState } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
+import { useRouter } from "next/navigation";
+import { Box } from "@mui/material";
 
 const CategoryCard = ({ cardImage, cardTitle, urlPath, tooltipText }) => {
-  const [hovered, setHovered] = useState(false)
-  const router = useRouter()
+  const [hovered, setHovered] = useState(false);
+  const router = useRouter();
 
   const handleHover = () => {
-    setHovered(true)
+    setHovered(true);
   };
 
   const handleLeave = () => {
-    setHovered(false)
+    setHovered(false);
   };
 
   const calculateOverlayStyle = () => {
     return {
-      position: 'absolute',
+      position: "absolute",
       top: 0,
       left: 0,
-      width: '100%',
-      height: '100%',
-      backgroundColor: 'black',
-      opacity: hovered ? 0 : 0.6, 
-      transition: 'opacity 0.3s',
-    }
-  }
+      width: "100%",
+      height: "100%",
+      backgroundColor: "black",
+      opacity: hovered ? 0 : 0.6,
+      transition: "opacity 0.3s",
+    };
+  };
   return (
-    <box>
+    <Box>
       <Tooltip title={tooltipText} arrow>
         <Card
           style={{
             width: 250,
             height: 350,
-            position: 'relative',
-            overflow: 'hidden',
-            margin: '10px',
-            transition: 'transform 0.3s',
-            cursor: 'pointer'          
+            position: "relative",
+            overflow: "hidden",
+            margin: "10px",
+            transition: "transform 0.3s",
+            cursor: "pointer",
           }}
           onMouseEnter={handleHover}
           onMouseLeave={handleLeave}
           onClick={() => router.push(urlPath)}
         >
-             <box style={calculateOverlayStyle()}></box>
+          <Box style={calculateOverlayStyle()}></Box>
           <CardMedia
             component="img"
             height="140"
             image={cardImage}
             alt={cardTitle}
             style={{
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover',
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
               zIndex: 0,
             }}
           />
-          <CardContent style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', color: 'white', fontFamily: '', fontSize: 'Your-Font-Size' }}>
-            <Typography gutterBottom variant="h5" component="box">
+          <CardContent
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              textAlign: "center",
+              color: "white",
+              fontFamily: "",
+              fontSize: "Your-Font-Size",
+            }}
+          >
+            <Typography gutterBottom variant="h5" component="Box">
               {cardTitle}
             </Typography>
           </CardContent>
         </Card>
       </Tooltip>
-    </box>
+    </Box>
   );
 };
-export default CategoryCard
+export default CategoryCard;
--- component/cards/ProductCard.js
+++ component/cards/ProductCard.js
@@ -1,91 +1,101 @@
-import React, { useState } from 'react'
-import Card from '@mui/material/Card'
-import CardMedia from '@mui/material/CardMedia'
-import CardContent from '@mui/material/CardContent'
-import CardActions from '@mui/material/CardActions'
-import IconButton from '@mui/material/IconButton'
-import Typography from '@mui/material/Typography'
-import FavoriteIcon from '@mui/icons-material/Favorite'
-import IosShareIcon from '@mui/icons-material/IosShare'
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
-import ArrowBackIcon from '@mui/icons-material/ArrowBack'
-import Menu from '@mui/material/Menu'
-import MenuItem from '@mui/material/MenuItem'
-import WhatsAppIcon from '@mui/icons-material/WhatsApp'
-import FacebookIcon from '@mui/icons-material/Facebook'
-import InstagramIcon from '@mui/icons-material/Instagram'
-import TwitterIcon from '@mui/icons-material/Twitter'
+import React, { useState } from "react";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import IosShareIcon from "@mui/icons-material/IosShare";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
-
-const RecipeReviewCard = ({ cardTitle = "BAŞLIK", cardDescription = 'DİĞER İÇERİKLER', imageUrls = [
-  '/images/m-1.jpg',
-  '/images/m-2.jpg',
-  '/images/m-3.jpg',
-  '/images/m-4.jpg',
-] }) => {
-  const [anchorEl, setAnchorEl] = useState(null)
-  const [isFavorite, setIsFavorite] = useState(false)
-  const [selectedImageIndex, setSelectedImageIndex] = useState(0)
-  const [isHovered, setIsHovered] = useState(false)
+const RecipeReviewCard = ({
+  cardTitle = "BAŞLIK",
+  cardDescription = "DİĞER İÇERİKLER",
+  imageUrls = [
+    "/images/m-1.jpg",
+    "/images/m-2.jpg",
+    "/images/m-3.jpg",
+    "/images/m-4.jpg",
+  ],
+}) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+  const [isHovered, setIsHovered] = useState(false);
 
   const handleClick = (event) => {
-    setAnchorEl(event.currentTarget)
+    setAnchorEl(event.currentTarget);
   };
 
   const handleClose = () => {
-    setAnchorEl(null)
+    setAnchorEl(null);
   };
 
   const handleButtonClick = () => {
-    setIsFavorite(!isFavorite)
+    setIsFavorite(!isFavorite);
   };
 
   const handleExpandClick = () => {
-    setSelectedImageIndex((prevIndex) => (prevIndex + 1) % imageUrls.length)
+    setSelectedImageIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
   };
 
   const handlePrevClick = () => {
-    setSelectedImageIndex((prevIndex) => (prevIndex - 1 + imageUrls.length) % imageUrls.length)
+    setSelectedImageIndex(
+      (prevIndex) => (prevIndex - 1 + imageUrls.length) % imageUrls.length
+    );
   };
 
   const handleMouseEnter = () => {
-    setIsHovered(true)
+    setIsHovered(true);
   };
 
   const handleMouseLeave = () => {
-    setIsHovered(false)
-  }
-
-
+    setIsHovered(false);
+  };
 
   return (
     <Card
-      sx={{ maxWidth: 300, marginLeft: 'auto', marginRight: 'auto', marginTop: 10,cursor: 'pointer'  }}
+      sx={{
+        maxWidth: 300,
+        marginLeft: "auto",
+        marginRight: "auto",
+        marginTop: 10,
+        cursor: "pointer",
+      }}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
       <CardMedia
         component="div"
         sx={{
-          position: 'relative',
-          overflow: 'hidden',
+          position: "relative",
+          overflow: "hidden",
           maxHeight: 400,
         }}
       >
         <img
           src={imageUrls[selectedImageIndex]}
-          style={{ width: '400px', height: '250px', display: 'block' }}
+          style={{ width: "400px", height: "250px", display: "block" }}
+          alt=""
         />
 
         <IconButton
           aria-label="prev"
           onClick={handlePrevClick}
           sx={{
-            position: 'absolute',
-            top: '50%',
-            left: '10px',
-            transform: 'translateY(-50%)',
-            backgroundColor: 'rgba(255, 255, 255, 0.7)',
+            position: "absolute",
+            top: "50%",
+            left: "10px",
+            transform: "translateY(-50%)",
+            backgroundColor: "rgba(255, 255, 255, 0.7)",
           }}
         >
           <ArrowBackIcon />
@@ -94,22 +104,27 @@ const RecipeReviewCard = ({ cardTitle = "BAŞLIK", cardDescription = 'DİĞER İ
           aria-label="next"
           onClick={handleExpandClick}
           sx={{
-            position: 'absolute',
-            top: '50%',
-            right: '10px',
-            transform: 'translateY(-50%)',
-            backgroundColor: 'rgba(255, 255, 255, 0.7)',
+            position: "absolute",
+            top: "50%",
+            right: "10px",
+            transform: "translateY(-50%)",
+            backgroundColor: "rgba(255, 255, 255, 0.7)",
           }}
         >
           <ArrowForwardIcon />
         </IconButton>
         {isHovered && (
           <>
-            <CardActions sx={{ position: 'absolute', top: 0, right: 0 }}>
+            <CardActions sx={{ position: "absolute", top: 0, right: 0 }}>
               <IconButton onClick={handleClick}>
-                <IosShareIcon style={{ color: 'white' }} />
+                <IosShareIcon style={{ color: "white" }} />
               </IconButton>
-              <Menu anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+              <Menu
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
                 <MenuItem onClick={handleClose}>
                   <WhatsAppIcon /> WhatsApp
                 </MenuItem>
@@ -124,9 +139,12 @@ const RecipeReviewCard = ({ cardTitle = "BAŞLIK", cardDescription = 'DİĞER İ
                 </MenuItem>
               </Menu>
             </CardActions>
-            <CardActions sx={{ position: 'absolute', bottom: 0, right: 0 }}>
-              <IconButton aria-label="add to favorites" onClick={handleButtonClick}>
-                <FavoriteIcon style={{ color: isFavorite ? 'red' : 'white' }} />
+            <CardActions sx={{ position: "absolute", bottom: 0, right: 0 }}>
+              <IconButton
+                aria-label="add to favorites"
+                onClick={handleButtonClick}
+              >
+                <FavoriteIcon style={{ color: isFavorite ? "red" : "white" }} />
               </IconButton>
             </CardActions>
           </>
@@ -145,4 +163,4 @@ const RecipeReviewCard = ({ cardTitle = "BAŞLIK", cardDescription = 'DİĞER İ
   );
 };
 
-export default RecipeReviewCard
+export default RecipeReviewCard;
