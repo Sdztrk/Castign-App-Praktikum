94dc08d321cd7587f7525dba359c1c007a0b8645
Hdemiribram <passoatacc5862g@mail.com>
2024-02-12 16:16:16 +0300

    profil fotoğrafı eklediği zaman sayfa yenilenmeden navbara düşümüyordu global statei günceleyerek sorun giderildi

--- component/layout/navbar.js
+++ component/layout/navbar.js
@@ -1,45 +1,48 @@
-
 import React, { useState, useEffect, useContext } from "react";
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import MovieOutlinedIcon from '@mui/icons-material/MovieOutlined';
-import { useRouter } from 'next/navigation'
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import MovieOutlinedIcon from "@mui/icons-material/MovieOutlined";
+import { useRouter } from "next/navigation";
 import { mainColor } from "@/constants/Colors";
-import API from '@/helpers/ApiBuilder';
-import AppContext from '@/AppContext';
-import Cookies from 'js-cookie';
-import { BackendMediaPath } from '@/constants/BackendValues';
+import API from "@/helpers/ApiBuilder";
+import AppContext from "@/AppContext";
+import Cookies from "js-cookie";
+import { BackendMediaPath } from "@/constants/BackendValues";
 import DropdownMain from "../menuItems/DropdownMain";
 
-
-
-const pages = ['Products', 'Pricing', 'Blog'];
+const pages = ["Products", "Pricing", "Blog"];
 
 function ResponsiveAppBar() {
-  const { setUserInfo } = useContext(AppContext);
-  const router = useRouter()
+  const { userInfo, setUserInfo } = useContext(AppContext);
+  const router = useRouter();
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
   const [user, setUser] = useState(null);
-  const [photoPath, setPhotoPath] = useState("/static/images/avatar/2.jpg")
+  const [photoPath, setPhotoPath] = useState("/static/images/avatar/2.jpg");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const curentUser = await API.get('get_current_user', Cookies.get("accessToken"));
+        const curentUser = await API.get(
+          "get_current_user",
+          Cookies.get("accessToken")
+        );
         if (curentUser) {
-          setUser(curentUser?.email)
-          setUserInfo({user: curentUser, loggedIn: curentUser?.email ? true : false})
-          setPhotoPath(BackendMediaPath + curentUser?.photo)
+          setUser(curentUser?.email);
+          setUserInfo({
+            user: curentUser,
+            loggedIn: curentUser?.email ? true : false,
+          });
+          setPhotoPath(BackendMediaPath + curentUser?.photo);
         }
       } catch (error) {
         console.error("Error:", error);
@@ -47,54 +50,61 @@ function ResponsiveAppBar() {
     };
     fetchData();
   }, []);
+  useEffect(() => {
+    if (userInfo?.user?.photo) {
+      setPhotoPath(BackendMediaPath + userInfo.user.photo);
+    }
+  }, [userInfo?.user?.photo]);
 
   const handleLogout = () => {
-    Cookies.remove('accessToken'); // Clear the authentication token
+    Cookies.remove("accessToken"); // Clear the authentication token
     setUser(null); // Clear user info from context
     setUserInfo({ user: null, loggedIn: false });
-    router.push('/login');
+    router.push("/login");
   };
 
   const handleOpenNavMenu = (event) => {
-    setAnchorElNav((prevAnchorEl) => (prevAnchorEl ? null : event.currentTarget));
+    setAnchorElNav((prevAnchorEl) =>
+      prevAnchorEl ? null : event.currentTarget
+    );
   };
 
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
-  }
+  };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
-  }
+  };
 
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
 
   const redirectToLogin = () => {
-    router.push("/login")
-  }
+    router.push("/login");
+  };
   const redirectToRegister = () => {
-    router.push("/register")
-  }
+    router.push("/register");
+  };
 
   return (
     <AppBar component="nav" sx={{ backgroundColor: mainColor }}>
-      <Container maxWidth="xl" >
+      <Container maxWidth="xl">
         <Toolbar disableGutters>
           <img
-            src='/logoy.png'
-            alt='Topfıyt'
+            src="/logoy.png"
+            alt="Topfıyt"
             onClick={() => router.push("/")}
             style={{
-              width: '40px',
-              height: '50px',
-              marginRight: '1px',
-              cursor: 'pointer' // Add this line
+              width: "40px",
+              height: "50px",
+              marginRight: "1px",
+              cursor: "pointer", // Add this line
             }}
           />
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -107,17 +117,18 @@ function ResponsiveAppBar() {
             </IconButton>
             <Box
               anchorEl={anchorElNav}
-              sx={{position:"absolute", top:57, left:-15}}
+              sx={{ position: "absolute", top: 57, left: -15 }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
-
             >
               {Boolean(anchorElNav) && (
                 <DropdownMain onClose={handleCloseNavMenu} />
               )}
             </Box>
           </Box>
-          <MovieOutlinedIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <MovieOutlinedIcon
+            sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
+          />
           <Typography
             variant="h5"
             noWrap
@@ -125,60 +136,58 @@ function ResponsiveAppBar() {
             href="/"
             sx={{
               mr: 2,
-              display: { xs: 'flex', md: 'none' },
+              display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: 'monospace',
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
             MM
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex'}, pt:2 }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" }, pt: 2 }}>
             <DropdownMain />
           </Box>
           {!user ? (
             <>
               <MenuItem onClick={redirectToLogin}>
-                <Typography textAlign="center" >Giriş Yap</Typography>
+                <Typography textAlign="center">Giriş Yap</Typography>
               </MenuItem>
               <MenuItem onClick={redirectToRegister}>
-                <Typography textAlign="center" >Kayıt Ol</Typography>
+                <Typography textAlign="center">Kayıt Ol</Typography>
               </MenuItem>
             </>
           ) : (
             <Box sx={{ flexGrow: 0 }}>
               <Tooltip title="Open settings">
                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt="Remy Sharp" src={photoPath} />
+                  <Avatar alt="kullanıcı fotoğrafı" src={photoPath} />
                 </IconButton>
               </Tooltip>
               <Menu
-                sx={{ mt: '45px' }}
+                sx={{ mt: "45px" }}
                 id="menu-appbar"
                 anchorEl={anchorElUser}
                 anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
+                  vertical: "top",
+                  horizontal: "right",
                 }}
                 keepMounted
                 transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
+                  vertical: "top",
+                  horizontal: "right",
                 }}
                 open={Boolean(anchorElUser)}
                 onClose={handleCloseUserMenu}
               >
-
-                <MenuItem onClick={() => router.push('/profile')}>
+                <MenuItem onClick={() => router.push("/profile")}>
                   <Typography textAlign="center">Profile</Typography>
                 </MenuItem>
                 <MenuItem onClick={() => handleLogout()}>
                   <Typography textAlign="center">Çıkış Yap</Typography>
                 </MenuItem>
-
               </Menu>
             </Box>
           )}
@@ -187,4 +196,4 @@ function ResponsiveAppBar() {
     </AppBar>
   );
 }
-export default ResponsiveAppBar
+export default ResponsiveAppBar;
